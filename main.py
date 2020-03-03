11a660e0f55b3a850c555c977a9e2fe58a4b46d1
Lucas Cantos <lucas.cantos@somarmeteorologia.com.br>
2020-03-02 21:00:44 -0300

    Using main for testing

--- main.py
+++ main.py
@@ -4,63 +4,7 @@ import math
 
 # pegar ttask e umax
 
-# loop nas demais linhas
-
-
-# Remove Users
-
-# Remove Server
-
-# Add server
-
-# Add user
-
-
-umax = 2
-ttask = 4
-
-server_list = {
-    'user_count': [],
-    'ticks_used': [],
-    'user_list': []
-}
-
-user_list = {
-    'first_tick':[],
-    'server_id': [],
-
-}
-
-tick_data = {
-    'servers_active':[],
-}
 users_input = [1,3,0,1,0,1]
-
-
-cost_count = [5,4,4]
-
-def remove_users():
-    pass
-
-def add_users(users_number, server):
-    pass
-
-def available_servers(server_count):
-    servers = []
-    for server_id in range(server_count):
-        if server_list['user_count'][server_id] <= umax:
-            servers.append(server_id)
-
-    return []
-
-def remove_server():
-    pass
-
-def add_server():
-    pass 
-
-def tick_update():
-    pass
 
 # Make a list with machines which will last only ttask
 n_machines = lambda x: math.floor(x/umax)
