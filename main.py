6fbe37346414e4b1946df5b885b3338b7ac567d9
Lucas Cantos <lucas.cantos@somarmeteorologia.com.br>
2020-03-03 14:08:10 -0300

    Output done. Cleaning up

--- main.py
+++ main.py
@@ -3,17 +3,31 @@ from src.load_balance import LoadBalance
 import numpy as np
 
 
-input_path = 'input/input.txt'
-input_data = open_file(input_path)
-new_balance = LoadBalance(input_data[0], input_data[1], input_data[2:])
-total_cost = 0
-output_path = 'output/output.txt'
-x=[]
-for j, vm in enumerate(new_balance.load_balance()):
-    for i, users in enumerate(vm['connections']):
-        if j <= 0:
-            x.append([users])
-        else:
-            x[i].append(users)
+if __name__ == "__main__":
+    # Open file and load data
+    input_path = 'input/input.txt'
+    input_data = open_file(input_path)
 
-print(x)
+
+    # Processing
+    new_balance = LoadBalance(input_data[0], input_data[1], input_data[2:])
+
+    # Making output
+    total_cost = 0
+    output_path = 'output/output.txt'
+    final_output=[]
+
+    for vm in new_balance.load_balance():
+        for i, users in enumerate(vm['connections']):
+            if total_cost <= 0:
+                final_output.append([users] if users > 0 else [])
+            else:
+                final_output[i].append(users) if users > 0 else None
+        total_cost += vm['cost']
+        
+    if len(final_output[-1]) == 0:
+        final_output[-1].append(0)
+
+    final_output.append([total_cost])
+    
+    save_file(output_path, final_output)
--- src/helpers.py
+++ src/helpers.py
@@ -1,6 +1,8 @@
 import numpy as np
+
 def open_file(path):
     return np.loadtxt(path, int)
 
-def save_file():
-    pass
+def save_file(path, data):
+    with open(path,"w") as f:
+        f.write("\n".join(",".join(map(str, x)) for x in data))
